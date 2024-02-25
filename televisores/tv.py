b2753e327722bb64b7725adc8c39537626196e75
Jonathan <joosoriore@unal.edu.co>
2024-02-25 15:27:49 -0500

    1

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,14 +1,3 @@
-class Marca:
-
-    def __init__(self,nombre):
-        self._nombre=nombre
-
-    def setNombre(self,nombre):
-        self.nombre=nombre
-
-    def getNombre(self):
-        return self.nombre
-    
 class TV:
     numTV=0
     def __init__ (self, marca, estado):
@@ -95,38 +84,3 @@ class TV:
         if self._estado and self._canal >= 2:
 
             self._canal -= 1
-
-           
-
-class Control:
-
-    def setTv(self, tv):
-        self.tv=tv
-    
-    def getTv(self):
-        return self.tv
-
-    def turnOn(self):
-        self.tv.turnOn()
-    
-    def turnOff(self):
-        self.tv.turnOff()
-
-    def canalUp(self):
-        self.tv.canalUp()
-
-    def canalDown(self):
-        self.tv.canalDown()
-
-    def volumenUp(self):
-        self.tv.volumenUp()
-
-    def volumenDown(self):
-        self.tv.volumenDown()
-    
-    def setCanal(self):
-        self.tv.setCanal()
-
-    def enlazar(self, tv):
-        self.tv=tv
-        tv.setControl(self)
